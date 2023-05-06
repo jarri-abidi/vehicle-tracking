5f0e7d0c1c5a7f25a0049c9117f9e12efaf677d6
Faisal Nisar <fnisar394@protonmail.com>
2023-05-06 20:23:58 +0500

    added migrations files

---
+++ postgres/migrations/000001_create_tasks_table.up.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS trips (
+   trip_id VARCHAR(50) NOT NULL,
+   car_id INTEGER NOT NULL,
+   driver_id INTEGER NOT NULL,
+   car_number VARCHAR(50) NOT NULL,
+   device_id VARCHAR(50) NOT NULL,
+   trip_active INTEGER NOT NULL,
+   start_message_id VARCHAR(50) NOT NULL,
+   start_date VARCHAR(20) NOT NULL,
+   start_latitude FLOAT NOT NULL,
+   start_longitude FLOAT NOT NULL,
+   start_odo FLOAT NOT NULL,
+   stop_message_id VARCHAR(50) NOT NULL,
+   stop_date VARCHAR(20) NOT NULL,
+   stop_latitude FLOAT NOT NULL,
+   stop_longitude FLOAT NOT NULL,
+   stop_odo FLOAT NOT NULL,
+   trip_duration INTEGER NOT NULL,
+   trip_distance FLOAT NOT NULL,
+   trip_duration_night INTEGER NOT NULL,
+   trip_distance_night INTEGER NOT NULL
+);
