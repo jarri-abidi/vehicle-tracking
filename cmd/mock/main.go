e2edc1de6f59bb7f1e09862a4fba77f7c838547e
Faisal Nisar <fnisar394@protonmail.com>
2023-05-18 08:40:01 +0500

    refactored mock server

--- cmd/mock/main.go
+++ cmd/mock/main.go
@@ -1,42 +1,23 @@
 package main
 
 import (
-	"context"
 	"flag"
+	"log"
 	"net/http"
-	"os"
-	"os/signal"
-
-	"github.com/go-kit/log"
 )
 
 func main() {
-	logger := log.NewJSONLogger(os.Stderr)
-	defer logger.Log("msg", "terminated")
 
 	addr := flag.String("addr", ":56000", "listen addr")
 	flag.Parse()
 
 	mux := http.NewServeMux()
-	mux.Handle("/", NewMockServer())
-	server := &http.Server{
-		Addr:    *addr,
-		Handler: mux,
-	}
+	mux.HandleFunc("/trips", handleListTrips)
+	mux.HandleFunc("/locations", handleListLocations)
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
+	log.Printf("transport=http, address=%s, msg=listening\n", *addr)
 
-	go func() {
-		logger.Log("transport", "http", "address", *addr, "msg", "listening")
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
-			logger.Log("transport", "http", "address", *addr, "msg", "failed", "err", err)
-			sig <- os.Interrupt // trigger shutdown of other resources
-		}
-	}()
-
-	logger.Log("received", <-sig, "msg", "terminating")
-	if err := server.Shutdown(context.Background()); err != nil {
-		logger.Log("msg", "could not shutdown http mock server", "err", err)
+	if err := http.ListenAndServe(*addr, mux); err != http.ErrServerClosed {
+		log.Printf("transport=http, address=%s, msg=failed, err=%v\n", *addr, err)
 	}
 }
